465f8c3b8e7b178870f54784f6bf651cdd5b47fc
chenchang320 <wx18237216619@163.com>
2026-07-31 16:15:07 +0800

    Refactor data paths and rq.fit.scad parameters
    Updated file paths for data input and output. Adjusted parameters for rq.fit.scad function calls.

--- Code/4.Simulated with a vine copula.R
+++ Code/4.Simulated with a vine copula.R
@@ -16,7 +16,7 @@ library(TSP)
 library(rvinecopulib)
 library(quantreg)
 library(fPortfolio)
-data1=read_excel('/Users/mac/Desktop/最初的沪深300中的158支个股股票数据.xlsx')
+data1=read_excel('/Users/mac/Desktop/GitHub-Englishi version/data/Simulation dataset.xlsx')
 select_data=data1[c(3:2676),-1]
 colnames(select_data)=c(1:158)
 select_data=select_data[,c(-23,-74,-133)]
@@ -56,8 +56,8 @@ U=array(0,dim = c(2673,130))
 spec1=ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),mean.model = list(armaOrder = c(0, 0), arfima = FALSE), distribution.model = "ged")
 for (i in 1:130) {
   garch[[i]]=ugarchfit(spec = spec1, data=f[,i],out.sample=0,solver="solnp",solver.control=list(trace=0))
-  sigma[,i]<-as.numeric(sigma(garch[[i]]))#提取条件标准差#
-  z[,i]<-residuals(garch[[i]],standardize=TRUE)#提取标准化残差#
+  sigma[,i]<-as.numeric(sigma(garch[[i]]))
+  z[,i]<-residuals(garch[[i]],standardize=TRUE)
   fit[[i]]=GNG_fit(z[,i], start = c(break1 = -2, break2 =1.5, mean = 0, sd =1,shape1 = 0.1, shape2= 0.1))
   CDF[[i]]=function(x){mistr::p(distribution(fit[[i]]),x)}
   inverse_CDF[[i]]<-function(x){mistr::q(distribution(fit[[i]]),x)}
@@ -71,87 +71,24 @@ ES=matrix(0,974,18)
 beta_end=array(0,dim = c(130,974,3))
 
 dataf<-array(0,dim<-c(1700,130,974))
-for (j in 16:399) {
+
+for (j in 967:974) {
   dataf[,,j]<-U[j:(j+1699),]
   RVINE<-RVineStructureSelect(dataf[,,j],c(1),type=0)
+  set.seed(321)
   Sim=RVineSim(1000,RVINE)
   V=array(0,dim = c(1000,130))
   ZZ=array(0,dim = c(1000,130))
-  #consigma=array(0,dim = c(2673,132))
   cancha=array(0,dim = c(1000,130))
   r=array(0,dim = c(1000,130))
   for (m in 1:130) {
     V[,m]=Sim[,m]
     ZZ[,m]=inverse_CDF[[m]](V[,m])
-    #consigma[,m]=as.numeric(sigma(ugarchfit(spec = spec1, data=f[,m],out.sample=0,solver="solnp",solver.control=list(trace=0))))
     cancha[,m]=ZZ[,m]*sigma[j+1699,m]
     r[,m]=cancha[,m]+coef(garch[[m]])[[1]]
     
   }
   
-  x=matrix(0,1000,130)
-  x[,1]=1
-  for (n in 2:130) {
-    for (p in 1:1000) {
-      x[p,n]=r[p,1]-r[p,n]
-      
-    }
-  } 
-  I=matrix(0,130,130)
-  for (q in 1:130) {
-    for (s in 1:130) {
-      if (q==s){
-        I[q,s]=1
-      }
-      
-    }
-  }
-  scad_penalty=function(beta_zero, lambda_val, a_val){
-    is_linear = (beta_zero <= lambda_val)
-    is_quadratic=(beta_zero > lambda_val)
-    #is_quadratic = (lambda_val < abs(beta_zero) & abs(beta_zero) <= a_val * lambda_val)
-    #is_constant = (a_val * lambda_val) < abs(beta_zero)
-    
-    linear_part = lambda_val * is_linear
-    quadratic_part = (a_val * lambda_val - beta_zero) / (a_val - 1) * is_quadratic
-    #constant_part = 0 * is_constant
-    p_der = linear_part + quadratic_part #+ constant_part
-    return (p_der)
-  }
-  x_zhuan=t(x)%*%x
-  lambda_range <- seq(0.1, 1, by = 0.1)
-  alpha_range <- seq(3, 4, by = 0.1)
-  beta_zero=2.2
-  I_1=matrix(0,130,130)
-  min_GCV=1
-  for (lambda_val in lambda_range) {
-    for (a_val in alpha_range) {
-      beta_1=solve(x_zhuan + 1000 * (scad_penalty(beta_zero,lambda_val,a_val)/abs(beta_zero))*I) %*% t(x) %*% r[,1]
-      for (t in c(1:130)) {
-        for (w in c(1:130)) {
-          if(t==w){
-            I_1[t,w]=(scad_penalty(beta_1[t],lambda_val,a_val))/abs(beta_1[t])
-          }
-          
-        }
-        
-      }
-      p_x=x %*% solve(x_zhuan + 1000 * I_1) %*% t(x)
-      e_theta=sum(diag(p_x))
-      y_hat=x%*%beta_1
-      vector_1=r[,1]-y_hat
-      squared_num=0
-      for (num in vector_1) {
-        squared_num=squared_num+num^2
-      }
-      GCV_theta=squared_num/(1000*(1-e_theta/1000)^2)
-      if(GCV_theta<min_GCV){
-        best_lambda=lambda_val
-        best_alpha=a_val
-        min_GCV=GCV_theta
-      }
-    }
-  }
   beta_hat=matrix(0,1000,129)
   for (a in 2:130) {
     for (b in 1:1000) {
@@ -160,9 +97,9 @@ for (j in 16:399) {
     }
     
   }
-  g_1=rq.fit.scad(beta_hat,r[,1],tau = 0.01,alpha = best_alpha,lambda = best_lambda)
-  g_2=rq.fit.scad(beta_hat,r[,1],tau = 0.05,alpha = best_alpha,lambda = best_lambda)
-  g_3=rq.fit.scad(beta_hat,r[,1],tau = 0.1,alpha = best_alpha,lambda = best_lambda)
+  g_1=rq.fit.scad(beta_hat,r[,1],tau = 0.01,alpha = 3.7,lambda = 0.6)
+  g_2=rq.fit.scad(beta_hat,r[,1],tau = 0.05,alpha = 3.7,lambda = 0.6)
+  g_3=rq.fit.scad(beta_hat,r[,1],tau = 0.1,alpha = 3.7,lambda = 0.6)
   beta_end_1=matrix(c(1-sum(g_1[["coefficients"]]),g_1[["coefficients"]]),130,1)
   beta_end_2=matrix(c(1-sum(g_2[["coefficients"]]),g_2[["coefficients"]]),130,1)
   beta_end_3=matrix(c(1-sum(g_3[["coefficients"]]),g_3[["coefficients"]]),130,1)
@@ -172,7 +109,7 @@ for (j in 16:399) {
   beta_end[,j,1]=beta_end_1
   beta_end[,j,2]=beta_end_2
   beta_end[,j,3]=beta_end_3
-
+  
   
   
   VaR_ninetynine_1<-sort(y_end_1)[10]
@@ -271,13 +208,11 @@ for (j in 16:399) {
   ES[j,16]=ES_ewp_ninety
   ES[j,17]=ES_minvar_niniety
   ES[j,18]=ES_meanvar_ninety
-  write.csv(VaR, file = '/Users/mac/Desktop/VAR.csv', row.names = FALSE)
-  write.csv(ES, file = '/Users/mac/Desktop/CVAR.csv', row.names = FALSE)
-  write.csv(beta_end[,,1],file = '/Users/mac/Desktop/权重1.csv', row.names = FALSE)
-  write.csv(beta_end[,,2],file = '/Users/mac/Desktop/权重2.csv', row.names = FALSE)
-  write.csv(beta_end[,,3],file = '/Users/mac/Desktop/权重3.csv', row.names = FALSE)
   print(j)
+  
+ 
 }
-
+write.csv(ES, file = '/Users/mac/Desktop/GitHub-Englishi version/data/simulated CVaR with vine.csv', row.names = FALSE)
+write.csv(VaR, file = '/Users/mac/Desktop/GitHub-Englishi version/data/simulated VaR with vine.csv', row.names = FALSE)
 
 
