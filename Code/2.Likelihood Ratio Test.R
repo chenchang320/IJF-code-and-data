3c026cb9b390e2abc7ad336282bf1a9aae38a3ed
chenchang320 <wx18237216619@163.com>
2026-07-31 11:50:46 +0800

    Update file paths and improve GARCH model fitting

--- Code/2.Likelihood Ratio Test.R
+++ Code/2.Likelihood Ratio Test.R
@@ -11,7 +11,7 @@ library(timeSeries)
 library(fBasics)
 library(mistr)
 library(quantmod)
-data1=read_excel('/Users/mac/Desktop/git-hub/数据/10支上证行业指数.xlsx')
+data1=read_excel('/Users/mac/Desktop/GitHub-Englishi version/data/Ten industry dataset.xlsx')
 select_data=data1[c(1:2674),c(-1)]
 colnames(select_data)=c(1:10)
 
@@ -55,7 +55,7 @@ for (i in  1:10){
 }
 
 
-# 初始化容器
+
 garch = list()
 sigma = array(0, dim = c(2673, 10))
 z = array(0, dim = c(2673, 10))
@@ -64,17 +64,17 @@ CDF = list()
 inverse_CDF = list()
 U = array(0, dim = c(2673, 10))
 
-# 用于保存信息准则
+
 logLik_values_1 = numeric(10)
 
-# 定义GARCH(1,1)-ARMA(1,1)模型
+
 spec1 = ugarchspec(
   variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
   mean.model = list(armaOrder = c(1, 1), arfima = FALSE),
   distribution.model = "ged"
 )
 
-# 循环拟合10组数据
+
 for (i in 1:10) {
   garch[[i]] = ugarchfit(
     spec = spec1, 
@@ -84,14 +84,14 @@ for (i in 1:10) {
     solver.control = list(trace = 0)
   )
   
-  # 提取条件标准差与标准化残差
+
   sigma[, i] <- as.numeric(sigma(garch[[i]]))
   z[, i] <- residuals(garch[[i]], standardize = TRUE)
   
-  # 提取Log
+
   logLik_values_1[i] <- likelihood(garch[[i]])
   
-  # 拟合标准化残差的分布并计算CDF
+
   fit[[i]] = GNG_fit(z[, i], start = c(break1 = -2, break2 = 1.5, mean = 0, sd = 1, shape1 = 0.1, shape2 = 0.1))
   CDF[[i]] = function(x) { mistr::p(distribution(fit[[i]]), x) }
   inverse_CDF[[i]] = function(x) { mistr::q(distribution(fit[[i]]), x) }
@@ -101,7 +101,7 @@ for (i in 1:10) {
 
 
 
-# 初始化容器
+
 garch = list()
 sigma = array(0, dim = c(2673, 10))
 z = array(0, dim = c(2673, 10))
@@ -110,17 +110,17 @@ CDF = list()
 inverse_CDF = list()
 U = array(0, dim = c(2673, 10))
 
-# 用于保存信息准则
+
 logLik_values_0 = numeric(10)
 
-# 定义GARCH(1,1)
+
 spec1 = ugarchspec(
   variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
   mean.model = list(armaOrder = c(0, 0), arfima = FALSE),
   distribution.model = "ged"
 )
 
-# 循环拟合10组数据
+
 for (i in 1:10) {
   garch[[i]] = ugarchfit(
     spec = spec1, 
@@ -130,14 +130,14 @@ for (i in 1:10) {
     solver.control = list(trace = 0)
   )
   
-  # 提取条件标准差与标准化残差
+
   sigma[, i] <- as.numeric(sigma(garch[[i]]))
   z[, i] <- residuals(garch[[i]], standardize = TRUE)
   
-  # 提取Log
+
   logLik_values_0[i] <- likelihood(garch[[i]])
   
-  # 拟合标准化残差的分布并计算CDF
+
   fit[[i]] = GNG_fit(z[, i], start = c(break1 = -2, break2 = 1.5, mean = 0, sd = 1, shape1 = 0.1, shape2 = 0.1))
   CDF[[i]] = function(x) { mistr::p(distribution(fit[[i]]), x) }
   inverse_CDF[[i]] = function(x) { mistr::q(distribution(fit[[i]]), x) }
@@ -147,23 +147,21 @@ for (i in 1:10) {
 
 
 
-# 初始化 LRT 和 p 值
+
 LRT_values <- numeric(10)
 p_values <- numeric(10)
 
-# 假设你有无 ARMA 和有 ARMA 的对数似然值
-# logLik_values_0 是无 ARMA 动态模型的对数似然值
-# logLik_values_1 是有 ARMA 动态模型的对数似然值
+
 
 for (i in 1:10) {
-  # 计算似然比检验统计量 LRT
+
   LRT_values[i] <- -2 * (logLik_values_0[i] - logLik_values_1[i])
   
-  # 计算 p 值，假设自由度为 2（因为 ARMA(1,1) 增加了两个参数）
+
   p_values[i] <- 1 - pchisq(LRT_values[i], df = 2)
 }
 
-# 打印 LRT 统计量和 p 值
+
 print(LRT_values)
 print(p_values)
 
